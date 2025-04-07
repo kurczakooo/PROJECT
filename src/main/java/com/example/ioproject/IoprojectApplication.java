3cbfb9333cfb1eba716ef2e2a1fba08c47fce94b
Astesik <jakub.sikora30@wp.pl>
2025-04-07 17:43:05 +0200

    init

---
+++ src/main/java/com/example/ioproject/IoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ioproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ioproject/IoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ioproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
