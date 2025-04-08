680fe82d8b4b2ea97f6d7ab036df84bbe5638b2f
Astesik <jakub.sikora30@wp.pl>
2025-04-08 23:31:36 +0200

    basic models, auth, signin, signup and connecting with db postgresql

---
+++ src/main/java/com/example/ioproject/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.ioproject.repository;
+
+import com.example.ioproject.models.ERole;
+import com.example.ioproject.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
