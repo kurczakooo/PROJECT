62d9ebf9d4f7a68a9cf3cce7a540379287b3c4fa
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2025-05-06 06:39:59 +0200

    removed secret from code and created .env to store it

--- src/main/java/com/example/ioproject/IoprojectApplication.java
+++ src/main/java/com/example/ioproject/IoprojectApplication.java
@@ -2,11 +2,17 @@ package com.example.ioproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class IoprojectApplication {
 
     public static void main(String[] args) {
+
+        // Ładowanie zmiennych środowiskowych z pliku .env
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(IoprojectApplication.class, args);
     }
 
